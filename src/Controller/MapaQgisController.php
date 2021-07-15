caf0ac920641d0b4375a980d1731c3f19542cc83
pcerezo <cerezotrabajo7@correo.ugr.es>
2021-07-15 17:42:29 +0200

    Añadido cenit de cada medición al mapa

--- src/Controller/MapaQgisController.php
+++ src/Controller/MapaQgisController.php
@@ -35,19 +35,6 @@ class MapaQgisController extends AbstractController
         // Doy valores a los datos para el mapa
         $rutaDatosMapas = $this->getParameter('directorio_mapa_js')."/medicionesCielo_5.js";
 
-        /*$datos_js = '{
-            "type": "FeatureCollection",
-            "name": "medicionesCielo_5",
-            "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-            "features": [
-                { "type": "Feature", "properties": { "fid": "1", "cenit": 19.0, "nombre": "Carretera Cabra", "imagen": "images/foto360.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.710803906867161, 37.012163727462713 ] } },
-                { "type": "Feature", "properties": { "fid": "2", "cenit": 20.2, "nombre": "El Purche", "imagen": "images/foto360.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.503842193736335, 37.152616166748437 ] } },
-                { "type": "Feature", "properties": { "fid": "3", "cenit": 21.3, "nombre": "Hoya de la Mora", "imagen": "images/foto360.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.386826862759218, 37.093557861530513 ] } },
-                { "type": "Feature", "properties": { "fid": "4", "cenit": 21.4, "nombre": "Gorafe", "imagen": "images/foto360.jpg" }, "geometry": { "type": "Point", "coordinates": [ -3.05099455504029, 37.511294999081414 ] } },
-                { "type": "Feature", "properties": { "fid": "5", "cenit": 21.0, "nombre": "La Sagra", "imagen": "images/foto360.jpg" }, "geometry": { "type": "Point", "coordinates": [ -2.522884610561774, 37.979423547228635 ] } }
-            ]
-        }';*/
-
         // Accedemos a la base de datos para obtener las mediciones
         $entityManager = $this->getDoctrine()->getManager();
         $conn = $entityManager->getConnection();
@@ -58,11 +45,6 @@ class MapaQgisController extends AbstractController
         // Se ejecuta la sentencia SQL
         $sentencia = $conn->prepare($sql_datos);
         $sentencia->execute();
-        //$datos = $sentencia->fetchAll();
-
-        /*$sentencia = $conn->prepare($sql_filas);
-        $sentencia->execute();
-        $filas = $sentencia->fetchAll();*/
 
         // Escribo los datos js en el fichero de datos para el mapa
         $contenido = 'var json_medicionesCielo_5 = ';
@@ -81,14 +63,23 @@ class MapaQgisController extends AbstractController
 
         // Creamos un array con datos de las mediciones que se añadirá a "features"
         while ($datos = $sentencia->fetch()) {
-            $features = [array('type' => 'Feature', 'properties' => array('fid' => $fid, 'cenit' => 19.0, 'nombre' => $datos['localizacion'], 'imagen' => 'uploads/mediciones/'.$datos['grafico'].'/'.$datos['grafico'].'.png'), 'geometry' => array('type' => 'Point', 'coordinates' => [$datos['longitud'], $datos['latitud']]))];    
+            // A partir del id de la medición genérica obtenemos el cenit en 90º
+            $sql_cenit = "SELECT magnitud FROM medicion_individual WHERE generica_id={$datos['id']} and declinacion=90";
+            $sentencia_cenit = $conn->prepare($sql_cenit);
+            $sentencia_cenit->execute();
+            $cenit = $sentencia_cenit->fetch();
+
+            $features = [array('type' => 'Feature', 'properties' => array('fid' => $fid, 'cenit' => $cenit['magnitud'], 'nombre' => $datos['localizacion'], 'imagen' => 'uploads/mediciones/'.$datos['grafico'].'/'.$datos['grafico'].'.png'), 'geometry' => array('type' => 'Point', 'coordinates' => [$datos['longitud'], $datos['latitud']]))];    
             
             $fid = $fid+1;
+
+            // Se concatena el array actual con lo que haya en la parte de features del json
             $datos_js['features'] = array_merge($datos_js['features'], $features);
         }
 
         $contenido .= json_encode($datos_js);
 
+        // Se escribe en el archivo del cual lee qgis
         file_put_contents($rutaDatosMapas, $contenido);
 
         return $this->render('mapa_qgis/index.html.twig', [
